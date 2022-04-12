1d1a5a9983a311f9e47de44f7f0f7a87f20dd21b
Hyun Sung Cho <hcho774@gmail.com>
2022-04-12 12:18:26 -0400

    commit tree d851ea685e26c0ccfea7c7f0cf7cccd4a4d17bae

--- src/index.js
+++ src/index.js
@@ -1,5 +1,23 @@
 const init = () => {
-  
+    //grab form from DOM
+    const inputForm = document.querySelector("form");
+    //console.log(form); to check if we grab the form tag and what is inside
+    // we add event listener to 
+    inputForm.addEventListener("submit", (e) => {
+        e.preventDefault();
+        const value = e.target.children[1].value;
+    fetch(`http://localhost:3000/movies/${value}`)
+        .then(function(response) {
+            return response.json();
+        })
+        .then(data => {
+            const title = document.querySelector("section#movieDetails h4");
+            const summary = document.querySelector("section#movieDetails p")
+
+            title.innerText = data.title;
+            summary.innerText = data.summary;
+        });
+    });
 }
 
 document.addEventListener('DOMContentLoaded', init);
